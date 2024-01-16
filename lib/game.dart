8e26f8adfc35eec0353262fe23361ddcbe488297
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-16 16:39:38 -0300

    fix: :wrench: Correcao da camera do mapa

--- lib/game.dart
+++ lib/game.dart
@@ -1,4 +1,3 @@
-// game.dart
 import 'package:flutter/material.dart';
 import 'package:bonfire/bonfire.dart';
 import 'package:supreme_kai_world/actor/actor.dart';
@@ -6,21 +5,27 @@ import 'package:supreme_kai_world/actor/actor.dart';
 class Game extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BonfireWidget(
-      showCollisionArea: false,
-      joystick: Joystick(
-        directional: JoystickDirectional(),
-      ), // required
-      map: WorldMapByTiled(
-        TiledReader.asset('tiled/map.json'),
-      ),
-      player: Actor(Vector2(400, 480), actorType: 'samurai'),
-      cameraConfig:CameraConfig(
-        moveOnlyMapArea: true, 
-        movementWindow: Vector2(50,50),
-        speed: 1.0,
-        zoom:  3.5,
-        startFollowPlayer: true,
+    return Material(
+      child: Stack(
+        children: [
+          BonfireWidget(
+            showCollisionArea: false,
+            joystick: Joystick(
+              directional: JoystickDirectional(),
+            ),
+            map: WorldMapByTiled(
+              TiledReader.asset('tiled/map.json'),
+            ),
+            player: Actor(Vector2(400, 480), actorType: 'princess'),
+            cameraConfig:CameraConfig(
+              moveOnlyMapArea: true, 
+              movementWindow: Vector2(36,36),
+              speed: 2.0,
+              zoom:  3.5,
+              startFollowPlayer: true,
+            ),
+          ),
+        ],
       ),
     );
   }
