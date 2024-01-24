561eb299c427349f8dd8934f1300dfc691a63739
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-23 21:39:33 -0300

    feat: :video_game: Adicao das placas de aviso ao mapa

--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,4 @@
+import 'package:bonfire/tiled/builder/tiled_world_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:bonfire/bonfire.dart';
 import 'package:supreme_kai_world/actor/actor.dart';
@@ -14,37 +15,13 @@ class Game extends StatelessWidget {
       child: Stack(
         children: [
           BonfireWidget(
-            showCollisionArea: true,
+            showCollisionArea: false,
             joystick: Joystick(
               directional: JoystickDirectional(),
             ),
             map: WorldMapByTiled(
               TiledReader.asset('tiled/map.json'),
-              objectsBuilder: {
-                'frog_statue': (p) => InteractiveDecoration(
-                      'decorations/frog_statue.png',
-                      context: context,
-                      textureSize: Vector2(32.0, 32.0),
-                      onCollisionAction: () => {Navigator.pop(context)},
-                      elementPosition: p.position,
-                    ),
-                'monk_statue': (p) => InteractiveDecoration(
-                      'decorations/monk_statue.png',
-                      context: context,
-                      textureSize: Vector2(32.0, 32.0),
-                      onCollisionAction: () => {debugPrint("entrou monge")},
-                      elementPosition: p.position,
-                      onCollisionEndAction: () => {debugPrint("saiu monge")}
-                    ),
-                'warrior_statue': (p) => InteractiveDecoration(
-                      'decorations/warrior_statue.png',
-                      context: context,
-                      amount: 4,
-                      textureSize: Vector2(32.0, 32.0),
-                      onCollisionAction: () => {Navigator.pop(context)},
-                      elementPosition: p.position,
-                    ),
-              },
+              objectsBuilder: iteractiveObjects(context),
             ),
             player: Actor(Vector2(400, 480), actorType: actorType),
             cameraConfig: CameraConfig(
@@ -57,6 +34,50 @@ class Game extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+
+  Map<String, ObjectBuilder> iteractiveObjects(BuildContext context) {
+    return {
+      'frog_statue': (p) => InteractiveDecoration(
+            'decorations/frog_statue.png',
+            context: context,
+            textureSize: Vector2(32.0, 32.0),
+            onCollisionAction: () => {Navigator.pop(context)},
+            elementPosition: p.position,
+          ),
+      'monk_statue': (p) => InteractiveDecoration(
+            'decorations/monk_statue.png',
+            context: context,
+            textureSize: Vector2(32.0, 32.0),
+            onCollisionAction: () => {debugPrint("entrou monge")},
+            elementPosition: p.position,
+            onCollisionEndAction: () => {debugPrint("saiu monge")}
+          ),
+      'warrior_statue': (p) => InteractiveDecoration(
+            'decorations/warrior_statue.png',
+            context: context,
+            amount: 4,
+            textureSize: Vector2(32.0, 32.0),
+            onCollisionAction: () => {Navigator.pop(context)},
+            elementPosition: p.position,
+          ),
+      'warrior_info': (p) => infoBoardDefault(context, p),
+      'monk_info': (p) => infoBoardDefault(context, p),
+      'frog_info': (p) => infoBoardDefault(context, p),
+    };
+  }
+
+  InteractiveDecoration infoBoardDefault(BuildContext context, TiledObjectProperties p) {
+    return InteractiveDecoration(
+      'decorations/info_board.png',
+      context: context,
+      amount: 4,
+      stepTime: 0.3,
+      textureSize: Vector2(32.0, 32.0),
+      onCollisionAction: () => {debugPrint("info")},
+      elementPosition: p.position,
     );
   }
 }
