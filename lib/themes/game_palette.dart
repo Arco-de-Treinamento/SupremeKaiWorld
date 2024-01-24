b5bc4dd5a16531ff9be4949436a01d8489cd9729
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-24 20:26:24 -0300

    feat: :art: Adicao da paleta de cores do game

---
+++ lib/themes/game_palette.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/// A classe [GamePalette] define todas as cores do game.
+/// A paleta de cores foi retirada diretamente do asset pack Ninja Adventure, disponível na itch.io.
+
+class GamePalette {
+  static Color primary = const Color(0xFFFF7E4A);
+  static Color onPrimary = const Color(0xFFF2EAF1);
+  static Color borderPrimary = const Color(0xFFFF3E24);
+
+  static Color secondary = const Color(0xFF141B1B);
+  static Color onSecondary = const Color(0xFFF2EAF1);
+  static Color borderSecondary = const Color(0xFF819380);
+
+  static Color primaryShadow = const Color(0xFF34322B);
+  static Color secondaryShadow = const Color(0xFF85AFBC);
+
+  static Color error = const Color(0xFFE0394C);
+  static Color onError = const Color(0xFF020202);
+
+  static Color background = const Color(0xFF686557);
+  static Color onBackground = const Color(0xFF020202);
+
+  static Color textPrimary = const Color(0xFFF2EAF1);
+  static Color textSecondary = const Color(0xFF020202);
+  static Color boxMenssage = const Color(0xFFf2EAF1);
+  static Color borderBoxMenssage = const Color(0xFFEA7C42);
+
+  static Color surface = const Color(0xFF141B1B);
+  static Color onSurface = const Color(0xFFF2EAF1);
+}
