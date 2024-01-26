53c89583b4ba910b3e70fce506d2a8933a7008bc
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-26 16:03:53 -0300

    refator: :hammer: Nomenclatura de variaveis do RetroTextButton

--- lib/util/retrotext_button.dart
+++ lib/util/retrotext_button.dart
@@ -3,27 +3,27 @@ import 'package:supreme_kai_world/themes/game_palette.dart';
 
 class RetroTextButton extends StatefulWidget {
   const RetroTextButton(
-    this.text, {
+    this.buttonText, {
     super.key,
     required this.textStyle,
-    required this.onTap,
+    required this.onPressed,
     this.shadowOffset = 8.0,
     this.verticalPadding = 0.0,
     this.horizontalPadding = 16.0,
-    this.animationDuration = 100,
-    this.backgroundColor = GamePalette.primary,
+    this.animationTime = 100,
+    this.buttonColor = GamePalette.primary,
     this.withAnimation = false,
     this.withShadow = true,
   });
 
-  final String text;
+  final String buttonText;
   final TextStyle textStyle;
-  final VoidCallback onTap;
+  final VoidCallback onPressed;
   final double shadowOffset;
   final double verticalPadding;
   final double horizontalPadding;
-  final int animationDuration;
-  final Color backgroundColor;
+  final int animationTime;
+  final Color buttonColor;
   final bool withAnimation;
   final bool withShadow;
 
@@ -33,41 +33,43 @@ class RetroTextButton extends StatefulWidget {
 
 class _RetroTextButtonState extends State<RetroTextButton>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _animation;
+  late final AnimationController _animationController;
+  late final Animation<double> _shadowAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: Duration(milliseconds: widget.animationDuration),
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: widget.animationTime),
       vsync: this,
     );
 
-    _animation = Tween(begin: 0.0, end: widget.shadowOffset).animate(_controller);
+    _shadowAnimation = Tween(begin: 0.0, end: widget.shadowOffset)
+        .animate(_animationController);
   }
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
-      onEnter: (event) => _controller.forward(),
-      onExit: (event) => _controller.reverse(),
+      onEnter: (event) => _animationController.forward(),
+      onExit: (event) => _animationController.reverse(),
       child: AnimatedBuilder(
-        animation: _animation,
+        animation: _shadowAnimation,
         builder: (_, child) {
           return Container(
             padding: EdgeInsets.symmetric(
                 vertical: widget.verticalPadding,
                 horizontal: widget.horizontalPadding),
             decoration: BoxDecoration(
-              color: widget.backgroundColor,
+              color: widget.buttonColor,
               boxShadow: [
                 widget.withShadow
                     ? BoxShadow(
                         color: GamePalette.primaryShadow,
                         blurRadius: 0,
                         offset: widget.withAnimation
-                            ? Offset(_animation.value, _animation.value)
+                            ? Offset(
+                                _shadowAnimation.value, _shadowAnimation.value)
                             : Offset(widget.shadowOffset, widget.shadowOffset),
                       )
                     : const BoxShadow(),
@@ -78,9 +80,9 @@ class _RetroTextButtonState extends State<RetroTextButton>
         },
         child: InkWell(
           hoverColor: Colors.transparent,
-          onTap: widget.onTap,
+          onTap: widget.onPressed,
           child: Text(
-            widget.text,
+            widget.buttonText,
             style: widget.textStyle,
           ),
         ),
@@ -90,7 +92,7 @@ class _RetroTextButtonState extends State<RetroTextButton>
 
   @override
   void dispose() {
-    _controller.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 }
