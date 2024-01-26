5a0a312be2f83de966b9949c1c128e8c1a1b975b
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-25 21:13:47 -0300

    refactor: :art: Refactor do game palette

--- lib/themes/game_palette.dart
+++ lib/themes/game_palette.dart
@@ -4,28 +4,28 @@ import 'package:flutter/material.dart';
 /// A paleta de cores foi retirada diretamente do asset pack Ninja Adventure, disponível na itch.io.
 
 class GamePalette {
-  static Color primary = const Color(0xFFFF7E4A);
-  static Color onPrimary = const Color(0xFFF2EAF1);
-  static Color borderPrimary = const Color(0xFFFF3E24);
+  static const Color primary = Color(0xFFFF7E4A);
+  static const Color onPrimary = Color(0xFFF2EAF1);
+  static const Color borderPrimary = Color(0xFFFF3E24);
 
-  static Color secondary = const Color(0xFF141B1B);
-  static Color onSecondary = const Color(0xFFF2EAF1);
-  static Color borderSecondary = const Color(0xFF819380);
+  static const Color secondary = Color(0xFF141B1B);
+  static const Color onSecondary = Color(0xFFF2EAF1);
+  static const Color borderSecondary = Color(0xFF819380);
 
-  static Color primaryShadow = const Color(0xFF34322B);
-  static Color secondaryShadow = const Color(0xFF85AFBC);
+  static const Color primaryShadow = Color(0xFF34322B);
+  static const Color secondaryShadow = Color(0xFF85AFBC);
 
-  static Color error = const Color(0xFFE0394C);
-  static Color onError = const Color(0xFF020202);
+  static const Color error = Color(0xFFE0394C);
+  static const Color onError = Color(0xFF020202);
 
-  static Color background = const Color(0xFF686557);
-  static Color onBackground = const Color(0xFF020202);
+  static const Color background = Color(0xFF686557);
+  static const Color onBackground = Color(0xFF020202);
 
-  static Color textPrimary = const Color(0xFFF2EAF1);
-  static Color textSecondary = const Color(0xFF020202);
-  static Color boxMenssage = const Color(0xFFf2EAF1);
-  static Color borderBoxMenssage = const Color(0xFFEA7C42);
+  static const Color textPrimary = Color(0xFFF2EAF1);
+  static const Color textSecondary = Color(0xFF020202);
+  static const Color boxMenssage = Color(0xFFf2EAF1);
+  static const Color borderBoxMenssage = Color(0xFFEA7C42);
 
-  static Color surface = const Color(0xFF141B1B);
-  static Color onSurface = const Color(0xFFF2EAF1);
+  static const Color surface = Color(0xFF141B1B);
+  static const Color onSurface = Color(0xFFF2EAF1);
 }
