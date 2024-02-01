3c32cce5f5982080302bf8ce0b2e5208cee4db37
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-02-01 16:59:38 -0300

    refactor: :hammer: Mudancas de posicao no ConstraineBox
    anteriormente o constrainedBox era adicionado ao LicenseWidget

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -37,34 +37,37 @@ class _AboutGameState extends State<AboutGame> {
       body: SingleChildScrollView(
         child: Container(
           margin: const EdgeInsets.all(28),
-          child: const Center(
-            child: Column(
-              children: [
-                LicenseInfoWidget(
-                  'Ninja Adventure Asset Pack',
-                  licenseContent:
-                      'Assets criados por Pixel-Boy e AAA disponíveis na itch.io, sob a licença Creative Commons Zero (CC0).',
-                  licenseTagPath: 'assets/images/tags/cc-zero.png',
-                  repositoryLink:
-                      'https://pixel-boy.itch.io/ninja-adventure-asset-pack',
-                ),
-                SizedBox(height: 24.0),
-                LicenseInfoWidget(
-                  'Pixelify Sans',
-                  licenseContent:
-                      'Fonte criada por Stefie Justprince/Typecalism Foundryline, disponível no Google Fonts e no GitHub, sob a licença OFL 1.1.',
-                  licenseTagPath: 'assets/images/tags/OFL.png',
-                  repositoryLink: 'https://github.com/eifetx/Pixelify-Sans',
-                ),
-                SizedBox(height: 24.0),
-                LicenseInfoWidget(
-                  'Arco de Treinamento',
-                  licenseContent:
-                      'Esse projeto visa agrupar todos os projetos produzidos em cursos e certificações a fim de facilitar o seu acesso por terceiros. Esse jogo foi criado para funcionar como um portifólio interativo e utiliza os assets do "Ninja Adventure Asset Pack" e a fonte "Pixelify Sans". Esse projeto está sob licença MIT.',
-                  licenseTagPath: 'assets/images/tags/MIT.png',
-                  repositoryLink: 'https://github.com/Arco-de-Treinamento',
-                ),
-              ],
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 600),
+              child: const Column(
+                children: [
+                  LicenseInfoWidget(
+                    'Ninja Adventure Asset Pack',
+                    licenseContent:
+                        'Assets criados por Pixel-Boy e AAA disponíveis na itch.io, sob a licença Creative Commons Zero (CC0).',
+                    licenseTagPath: 'assets/images/tags/cc-zero.png',
+                    repositoryLink:
+                        'https://pixel-boy.itch.io/ninja-adventure-asset-pack',
+                  ),
+                  SizedBox(height: 24.0),
+                  LicenseInfoWidget(
+                    'Pixelify Sans',
+                    licenseContent:
+                        'Fonte criada por Stefie Justprince/Typecalism Foundryline, disponível no Google Fonts e no GitHub, sob a licença OFL 1.1.',
+                    licenseTagPath: 'assets/images/tags/OFL.png',
+                    repositoryLink: 'https://github.com/eifetx/Pixelify-Sans',
+                  ),
+                  SizedBox(height: 24.0),
+                  LicenseInfoWidget(
+                    'Arco de Treinamento',
+                    licenseContent:
+                        'Esse projeto visa agrupar todos os projetos produzidos em cursos e certificações a fim de facilitar o seu acesso por terceiros. Esse jogo foi criado para funcionar como um portifólio interativo e utiliza os assets do "Ninja Adventure Asset Pack" e a fonte "Pixelify Sans". Esse projeto está sob licença MIT.',
+                    licenseTagPath: 'assets/images/tags/MIT.png',
+                    repositoryLink: 'https://github.com/Arco-de-Treinamento',
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/util/license_widget.dart
+++ lib/util/license_widget.dart
@@ -21,49 +21,46 @@ class LicenseInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 600),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            licenseTitle,
-            style: GameTextStyle.titleMedium.primary,
-          ),
-          const SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flexible(
-                child: Text(
-                  licenseContent,
-                  textAlign: TextAlign.justify,
-                  style: GameTextStyle.bodyTextMedium.primary,
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          licenseTitle,
+          style: GameTextStyle.titleMedium.primary,
+        ),
+        const SizedBox(height: 16.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+              child: Text(
+                licenseContent,
+                textAlign: TextAlign.justify,
+                style: GameTextStyle.bodyTextMedium.primary,
               ),
-              Container(
-                margin: const EdgeInsets.only(left: 24.0),
-                height: 36,
-                child: Image.asset(licenseTagPath),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              RetroTextButton(
-                'Ir para a página',
-                buttonColor: GamePalette.secondary,
-                textStyle: GameTextStyle.buttonSmall.primary,
-                withShadow: false,
-                onPressed: () => launchWebLink(repositoryLink),
-              ),
-            ],
-          ),
-        ],
-      ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 24.0),
+              height: 36,
+              child: Image.asset(licenseTagPath),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            RetroTextButton(
+              'Ir para a página',
+              buttonColor: GamePalette.secondary,
+              textStyle: GameTextStyle.buttonSmall.primary,
+              withShadow: false,
+              onPressed: () => launchWebLink(repositoryLink),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
