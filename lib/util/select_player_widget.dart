68cc9dd746256a95c2a0902328896b289762e256
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-01-31 21:06:57 -0300

    feat: :video_game: Criacao do widget de selecao do player

---
+++ lib/util/select_player_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:supreme_kai_world/actor/actor_card.dart';
+import 'package:supreme_kai_world/util/sprite_button.dart';
+
+class SelectPlayerWidget extends StatefulWidget {
+  final ValueChanged<String> onActorSelected;
+
+  const SelectPlayerWidget({
+    super.key,
+    required this.onActorSelected,
+  });
+
+  @override
+  _SelectPlayerWidgetState createState() => _SelectPlayerWidgetState();
+}
+
+class _SelectPlayerWidgetState extends State<SelectPlayerWidget> {
+  int _currentActorIndex = 0;
+  final double _sizeArrowSprite = 40.42;
+  final List<String> _actors = [
+    'cave_girl',
+    'inspector',
+    'princess',
+    'ninja',
+    'samurai'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SpriteButton(
+          'assets/images/items/arrow.png',
+          heightSprite: _sizeArrowSprite,
+          widthSprite: _sizeArrowSprite,
+          isFlippedHorizontally: true,
+          onPressed: selectPreviousPlayer,
+        ),
+        ActorCard(actor: _actors[_currentActorIndex], context: context),
+        const SizedBox(width: 8.0),
+        SpriteButton(
+          'assets/images/items/arrow.png',
+          heightSprite: _sizeArrowSprite,
+          widthSprite: _sizeArrowSprite,
+          onPressed: selectNextPlayer,
+        ),
+      ],
+    );
+  }
+
+  void selectNextPlayer() {
+    setState(() {
+      _currentActorIndex = (_currentActorIndex + 1) % _actors.length;
+      widget.onActorSelected(_actors[_currentActorIndex]);
+    });
+  }
+
+  void selectPreviousPlayer() {
+    setState(() {
+      _currentActorIndex = (_currentActorIndex - 1) % _actors.length;
+      widget.onActorSelected(_actors[_currentActorIndex]);
+    });
+  }
+}
